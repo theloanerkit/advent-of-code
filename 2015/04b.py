50cdab6c7c50646c5d766e77f821f745efb04a40
theloanerkit <126063546+theloanerkit@users.noreply.github.com>
2023-09-24 12:25:22 +0100

    day four solved

---
+++ 2015/04b.py
@@ -0,0 +1,19 @@
+import sys
+import hashlib
+sys.path.append("/home/kit/Documents/advent_of_code")
+import aoc
+
+data = aoc.read("2015","04","str")
+
+zeros = False
+i = 0
+
+while not zeros:
+    string = data + str(i)
+    string = string.encode()
+    result = str(hashlib.md5(string).hexdigest())
+    if result[:6] == "000000":
+        print(result)
+        print(i)
+        zeros = True
+    i+=1
