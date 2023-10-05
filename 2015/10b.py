55a4d726f1eae7aa6252a1f51ebe0761ceec6456
theloanerkit <126063546+theloanerkit@users.noreply.github.com>
2023-10-05 21:52:51 +0100

    day ten solved

---
+++ 2015/10b.py
@@ -0,0 +1,36 @@
+import sys
+sys.path.append("/home/kit/Documents/advent_of_code")
+import aoc
+
+def split_groups(string):
+    string_split = []
+    next_string = ""
+    for i in range(len(string)):
+        if next_string == "": # next string is empty, start a new next string
+            next_string+=string[i]
+        else:
+            if next_string[-1] == string[i]: # last char of next string is the same as the next char, keep adding
+                next_string+=string[i]
+            else: # next character is different
+                string_split.append(next_string)
+                next_string = ""
+                next_string+=string[i]
+    string_split.append(next_string)
+    return string_split
+
+def look_and_say(split_string):
+    new_string = ""
+    for elem in split_string:
+        new_string+=str(len(elem))
+        new_string+=elem[0]
+    return new_string
+
+data = aoc.read("2015","10","str")
+#print(data)
+#print(split_groups(data))
+
+for i in range(50):
+    split_data = split_groups(data)
+    data = look_and_say(split_data)
+
+print(len(data))
